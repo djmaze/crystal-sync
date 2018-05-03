114351b2db8201e0330aedb656a2a887bec8b12a
Martin Honermeyer <maze@strahlungsfrei.de>
2018-05-04 00:44:13 +0200

    Split tool into dump and load command
    It can now be used with files or regular unix pipes!

--- src/crystal-sync.cr
+++ src/crystal-sync.cr
@@ -4,94 +4,115 @@ require "../config/anonymization_config"
 require "admiral"
 
 class CrystalSync < Admiral::Command
-  define_argument database_source : String, required: true, description: "source database URL or filename"
-  define_argument database_target : String, required: true, description: "target database URL or filename"
-  define_help short: h, description: "Copies a source into a target database, while optionally anonymizing data.
-
-The source and target arguments have to be database URLs in the form of:
-
-    postgres://<user>:<password>@<host>:<port>/<database>
-    mysql://<user>:<password>@<host>:<port>/<database>"
+  define_help
 
   def run
-    unless db_uri?(arguments.database_source) && db_uri?(arguments.database_target)
-      STDERR.puts "Fatal: invalid database URI! Use either postgres:// or mysql:// scheme for both arguments"
-      exit 1
+    puts help
+  end
+
+  module DbHelpers
+    def db_uri?(string : String)
+      ["mysql", "postgres"].includes? URI.parse(string).scheme
     end
+  end
 
-    schema = IO::Memory.new 0       # FIXME Find better way to prevent compilation error
+  class DumpCommand < Admiral::Command
+    include DbHelpers
 
-    config = AnonymizationConfig.instance
-    anonymizer = Anonymizer.new(config)
-    packer = MessagePack::Packer.new
+    define_argument database_url : String, required: true, description: "source database URL"
+    define_help short: h, description: "Dumps a database while optionally anonymizing data."
 
-    dump_channel = Channel(Nil).new
-    Db.new arguments.database_source do |db|
-      spawn do
-        STDERR.puts "Dumping schema in the background"
-        schema = db.dump_schema
-        dump_channel.send nil
+    def run
+      unless db_uri?(arguments.database_url)
+        STDERR.puts "Fatal: invalid database URI! Use either postgres:// or mysql:// scheme"
+        exit 1
       end
 
-      db.tables.each do |table|
-        if anonymizer.skip_table?(table.name)
-          STDERR.puts "Skipping #{table.name}"
-        else
-          records = table.count
-          STDERR.puts "#{table.name}: #{records} records"
+      output = STDOUT
 
-          if records > 0
-            table_anonymizer = anonymizer.for_table(table)
-            TableSerializer.new(table, anonymizer: table_anonymizer).to_msgpack(packer)
+      config = AnonymizationConfig.instance
+      anonymizer = Anonymizer.new(config)
+      packer = MessagePack::Packer.new(output)
+
+      Db.new arguments.database_url do |db|
+        STDERR.puts "Dumping schema"
+        packer.write db.dump_schema.gets_to_end
+
+        db.tables.each do |table|
+          if anonymizer.skip_table?(table.name)
+            STDERR.puts "Skipping #{table.name}"
+          else
+            records = table.count
+            STDERR.puts "\n#{table.name}: #{records} records"
+
+            if records > 0
+              table_anonymizer = anonymizer.for_table(table)
+              TableSerializer.new(table, anonymizer: table_anonymizer).to_msgpack(packer)
+            end
           end
         end
       end
+      packer.write(:EOF)
+
+      STDERR.puts "\nDump finished!"
     end
-    packer.write(:EOF)
-    packed = packer.to_slice
+  end
 
-    STDERR.printf "Waiting for dump to finish.."
-    STDERR.flush
-    dump_channel.receive
-    STDERR.puts "\nExport finished!"
+  class LoadCommand < Admiral::Command
+    include DbHelpers
 
-    Db.new arguments.database_target do |db|
-      STDERR.puts "Recreating empty target database"
-      db.clear!
-    end
+    define_argument database_url : String, required: true, description: "target database URL"
+    define_help short: h, description: "Loads a database dump from a crystal-sync dump."
 
-    Db.new arguments.database_target do |db|
-      STDERR.puts "Loading schema"
-      db.load_schema(schema)
+    def run
+      unless db_uri?(arguments.database_url)
+        STDERR.puts "Fatal: invalid database URI! Use either postgres:// or mysql:// scheme"
+        exit 1
+      end
 
-      STDERR.puts "Loading data"
+      input = STDIN
 
-      loader = DataLoader.new(db)
+      Db.new arguments.database_url do |db|
+        STDERR.puts "Recreating empty target database"
+        db.clear!
+      end
 
-      puts "Deferring foreign-key constraints"
-      db.defer_fk_constraints do
-        last_table_name = ""
-        DeserializedData.from_msgpack(packed) do |deserialized|
-          table_name = deserialized.table_name
-          if table_name != last_table_name
-            STDERR.printf "\nLoading table #{deserialized.table_name}"
+      Db.new arguments.database_url do |db|
+        STDERR.puts "Loading schema"
+        packer = MessagePack::Unpacker.new(input)
+        buffer = IO::Memory.new 1024
+        buffer.write packer.read_string.to_slice
+        buffer.rewind
+        db.load_schema(buffer)
+
+        STDERR.puts "Loading data"
+
+        loader = DataLoader.new(db)
+
+        puts "Deferring foreign-key constraints"
+        db.defer_fk_constraints do
+          last_table_name = ""
+          DeserializedData.from_msgpack(input) do |deserialized|
+            table_name = deserialized.table_name
+            if table_name != last_table_name
+              STDERR.printf "\nLoading table #{deserialized.table_name}"
+              STDERR.flush
+              last_table_name = table_name
+            end
+
+            STDERR.printf(".")
             STDERR.flush
-            last_table_name = table_name
+            loader.load(deserialized)
           end
-
-          STDERR.printf(".")
-          STDERR.flush
-          loader.load(deserialized)
         end
       end
+
+      STDERR.puts "\nLoad finished!"
     end
-
-    STDERR.puts "\nImport finished!"
   end
 
-  def db_uri?(string : String)
-    ["mysql", "postgres"].includes? URI.parse(string).scheme
-  end
+  register_sub_command :dump, DumpCommand
+  register_sub_command :load, LoadCommand
 end
 
 CrystalSync.run
